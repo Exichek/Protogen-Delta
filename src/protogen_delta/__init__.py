38e33dc11fc3f2bc7524d4b8f55a91648aea4e1c
Exi <email@example.com>
2026-09-15 23:48:36 +0300

    Улучшены метаданные Python-пакета

--- src/protogen_delta/__init__.py
+++ src/protogen_delta/__init__.py
@@ -1,3 +1,8 @@
 """Protogen Delta package."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("protogen-delta")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
